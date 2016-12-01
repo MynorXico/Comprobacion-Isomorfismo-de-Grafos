ace9ddd9702661b37055eb217b2e3e11aa90be66
BMac9715 <bryan.macario12@gmail.com>
2016-12-01 15:08:53 -0600

    Parte Grafica - Primer Commit

--- ProyectoIsomorfismo/Form1.Designer.cs
+++ ProyectoIsomorfismo/Form1.Designer.cs
@@ -32,7 +32,14 @@
             this.btnGrafo2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnComprobar = new System.Windows.Forms.Button();
+            this.Grafos = new System.Windows.Forms.GroupBox();
+            this.btnDibujarGrafos = new System.Windows.Forms.Button();
+            this.pbGrafo1 = new System.Windows.Forms.PictureBox();
+            this.pbGrafo2 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            this.Grafos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrafo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrafo2)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGrafo1
@@ -69,7 +76,7 @@
             // btnComprobar
             // 
             this.btnComprobar.Enabled = false;
-            this.btnComprobar.Location = new System.Drawing.Point(243, 44);
+            this.btnComprobar.Location = new System.Drawing.Point(233, 44);
             this.btnComprobar.Name = "btnComprobar";
             this.btnComprobar.Size = new System.Drawing.Size(129, 23);
             this.btnComprobar.TabIndex = 2;
@@ -77,16 +84,57 @@
             this.btnComprobar.UseVisualStyleBackColor = true;
             this.btnComprobar.Click += new System.EventHandler(this.btnComprobar_Click);
             // 
+            // Grafos
+            // 
+            this.Grafos.Controls.Add(this.pbGrafo2);
+            this.Grafos.Controls.Add(this.pbGrafo1);
+            this.Grafos.Location = new System.Drawing.Point(22, 93);
+            this.Grafos.Name = "Grafos";
+            this.Grafos.Size = new System.Drawing.Size(556, 239);
+            this.Grafos.TabIndex = 3;
+            this.Grafos.TabStop = false;
+            this.Grafos.Text = "Grafos";
+            // 
+            // btnDibujarGrafos
+            // 
+            this.btnDibujarGrafos.Location = new System.Drawing.Point(368, 44);
+            this.btnDibujarGrafos.Name = "btnDibujarGrafos";
+            this.btnDibujarGrafos.Size = new System.Drawing.Size(131, 23);
+            this.btnDibujarGrafos.TabIndex = 4;
+            this.btnDibujarGrafos.Text = "Ver Grafos";
+            this.btnDibujarGrafos.UseVisualStyleBackColor = true;
+            // 
+            // pbGrafo1
+            // 
+            this.pbGrafo1.Location = new System.Drawing.Point(20, 19);
+            this.pbGrafo1.Name = "pbGrafo1";
+            this.pbGrafo1.Size = new System.Drawing.Size(250, 200);
+            this.pbGrafo1.TabIndex = 0;
+            this.pbGrafo1.TabStop = false;
+            // 
+            // pbGrafo2
+            // 
+            this.pbGrafo2.Location = new System.Drawing.Point(288, 19);
+            this.pbGrafo2.Name = "pbGrafo2";
+            this.pbGrafo2.Size = new System.Drawing.Size(250, 200);
+            this.pbGrafo2.TabIndex = 1;
+            this.pbGrafo2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 261);
+            this.ClientSize = new System.Drawing.Size(608, 349);
+            this.Controls.Add(this.btnDibujarGrafos);
+            this.Controls.Add(this.Grafos);
             this.Controls.Add(this.btnComprobar);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
+            this.Grafos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrafo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrafo2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -97,6 +145,10 @@
         private System.Windows.Forms.Button btnGrafo2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnComprobar;
+        private System.Windows.Forms.GroupBox Grafos;
+        private System.Windows.Forms.Button btnDibujarGrafos;
+        private System.Windows.Forms.PictureBox pbGrafo2;
+        private System.Windows.Forms.PictureBox pbGrafo1;
     }
 }
 
--- ProyectoIsomorfismo/Form1.cs
+++ ProyectoIsomorfismo/Form1.cs
@@ -14,8 +14,17 @@ namespace ProyectoIsomorfismo
 {
     public partial class Form1 : Form
     {
+        const int ANGULO_CIRCULO = 360;
+
+        private Bitmap grafo1;
+        private Bitmap grafo2;
+        private Graphics a;
+        private Graphics b;
+        private Coordenadas[] coordenadasVerticesG1;
+        private Coordenadas[] coordenadasVerticesG2;
         Grafo g1;
         Grafo g2;
+        
         public Form1()
         {
             InitializeComponent();
@@ -77,5 +86,142 @@ namespace ProyectoIsomorfismo
                 Reiniciar();
             }
         }
+
+        //Agrega MACA
+        private void DeterminarCoordenadas(int cantidadVertices, int radio, int centroY, int centroX)
+        {
+            coordenadasVertices = new Coordenadas[cantidadVertices];
+            double angulo = ANGULO_CIRCULO / cantidadVertices;
+            double anguloActual = 0;
+            int almacenarX = 0;
+            int almacenarY = 0;
+
+            for (int i = 0; i < cantidadVertices; i++)
+            {
+                almacenarX = CoordenadaX(radio, anguloActual, centroX);
+                almacenarY = CoordenadaY(radio, anguloActual, centroY);
+                coordenadasVertices[i] = new Coordenadas(almacenarX, almacenarY);
+                anguloActual += angulo;
+            }
+        }
+
+        private int CoordenadaY(int radio, double angulo, int centroY)
+        {
+            //Almacena la coordenada y del vertice 
+            int coordenadaY = 0;
+
+            switch ((int)angulo)
+            {
+                case 0:
+                    coordenadaY = centroY;
+                    break;
+                case 90:
+                    coordenadaY = centroY - radio;
+                    break;
+                case 180:
+                    coordenadaY = centroY;
+                    break;
+                case 270:
+                    coordenadaY = centroY + radio;
+                    break;
+
+                default:
+
+                    //Primer cuadrante
+                    if (angulo < 90 && angulo > 0)
+                    {
+                        coordenadaY = centroY;
+                        coordenadaY = coordenadaY - (int)(radio * Math.Sin((angulo) * (Math.PI / 180)));
+                        break;
+                    }
+
+                    //Segundo cuadrante
+                    if (angulo < 180 && angulo > 90)
+                    {
+                        coordenadaY = centroY;
+                        coordenadaY = coordenadaY - (int)(radio * Math.Sin((180 - angulo) * (Math.PI / 180)));
+                        break;
+                    }
+
+                    //Tercer cuadrante
+                    if (angulo < 270 && angulo > 180)
+                    {
+                        coordenadaY = centroY;
+                        coordenadaY = coordenadaY + (int)(radio * Math.Sin((angulo - 180) * (Math.PI / 180)));
+                        break;
+                    }
+
+                    //Cuarto cuadrante
+                    if (angulo < 360 && angulo > 270)
+                    {
+                        coordenadaY = centroY;
+                        coordenadaY = coordenadaY + (int)(radio * Math.Sin((360 - angulo) * (Math.PI / 180)));
+                        break;
+                    }
+                    break;
+            }
+
+            return coordenadaY;
+        }
+
+        private int CoordenadaX(int radio, double angulo, int centroX)
+        {
+            //Almacena el valor de la coordenada
+            int coordenadaX = 0;
+
+            switch ((int)angulo)
+            {
+                case 0:
+                    coordenadaX = centroX + radio;
+                    break;
+                case 90:
+                    coordenadaX = centroX;
+                    break;
+                case 180:
+                    coordenadaX = centroX - radio;
+                    break;
+                case 270:
+                    coordenadaX = centroX;
+                    break;
+
+                default:
+
+                    //Primer cuadrante
+                    if (angulo < 90 && angulo > 0)
+                    {
+                        coordenadaX = centroX;
+                        coordenadaX = coordenadaX + (int)(radio * Math.Sin((90 - angulo) * (Math.PI / 180)));
+                        break;
+                    }
+
+                    //Segundo cuadrante
+                    if (angulo < 180 && angulo > 90)
+                    {
+                        coordenadaX = centroX;
+                        coordenadaX = coordenadaX - (int)(radio * Math.Sin((90 - (180 - angulo)) * (Math.PI / 180)));
+                        break;
+                    }
+
+                    //Tercer cuadrante
+                    if (angulo < 270 && angulo > 180)
+                    {
+                        coordenadaX = centroX;
+                        coordenadaX = coordenadaX - (int)(radio * Math.Sin((90 - (angulo - 180)) * (Math.PI / 180)));
+                        break;
+                    }
+
+                    //Cuarto cuadrante
+                    if (angulo < 360 && angulo > 270)
+                    {
+                        coordenadaX = centroX;
+                        coordenadaX = coordenadaX + (int)(radio * Math.Sin((90 - (360 - angulo)) * (Math.PI / 180)));
+                        break;
+                    }
+                    break;
+            }
+
+            return coordenadaX;
+        }
+
     }
 }
